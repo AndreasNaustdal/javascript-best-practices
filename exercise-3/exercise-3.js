17a11a12625895a494148426e76dff53317bd4e1
Andreas Naustdal <and.epost@gmail.com>
2017-10-31 19:22:14 +0100

    Variable hoisting inside a function

--- exercise-3/exercise-3.js
+++ exercise-3/exercise-3.js
@@ -3,6 +3,7 @@ var myMessage = 'A message';
 function printStuff() {
   myMessage = 'Testing 123'; // changed
   console.log(myMessage);
+  var myMessage; // added
 }
 printStuff();
 console.log(myMessage); // changed
