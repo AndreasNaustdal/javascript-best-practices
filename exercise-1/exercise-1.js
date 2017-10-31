85910a7a03beffbcba1a3dc96b4ff4925ad18c07
Andreas Naustdal <and.epost@gmail.com>
2017-10-31 18:34:32 +0100

    fix bad brace style

--- exercise-1/exercise-1.js
+++ exercise-1/exercise-1.js
@@ -2,18 +2,15 @@ var x = 1;
 var y = 2;
 console.log(x + y);
 
-function myFancyFunction()
-{
+function myFancyFunction() {
   return "Important stuff";
 }
 
-function myOtherFunction()
-{
+function myOtherFunction() {
   // Do stuff
 }
 
-function myFunctionReturningAnObject()
-{
+function myFunctionReturningAnObject() {
   return {
     name: "Mad Max",
     age: 35
