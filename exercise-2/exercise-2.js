2f6d760baf963ac59c07fc7da100a4bac6f8f140
Andreas Naustdal <and.epost@gmail.com>
2017-10-31 18:37:55 +0100

    add example of loose equality

--- exercise-2/exercise-2.js
+++ exercise-2/exercise-2.js
@@ -1,1 +1,13 @@
 // Your code for exercise 2 here
+var a = 1;
+var b = '1';
+
+function areEqual(x, y) {
+  return x == y;
+}
+
+if(areEqual(a, b)) {
+  console.log('a and b are equal!');
+} else {
+  console.log('a and b are not equal :(');
+}
